baf43dba4b97920b56f73d8d1f0506c0e0121da6
Sara <sara.m.patyi@gmail.com>
2022-11-07 15:52:41 -0500

    initial code from spookyjam kickoff

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+import random
+
+app = flask.Flask(__name__)
+
+possible_images = [
+    "/static/farquaad.gif",
+    "/static/donkey-smile.gif",
+    "/static/pinocchio-ginger-bread.gif",
+]
+
+
+@app.route("/")
+def index():
+    img = random.choice(possible_images)
+    return flask.render_template(
+        "index.html",
+        spooky_image=img,
+    )
+
+
+@app.route("/swamp")
+def swamp():
+    return flask.render_template("swamp.html")
+
+
+app.run()
