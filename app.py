a330ffc34dd2c2ed9f8c171e558c2b140ee77f8a
Sara <sara.m.patyi@gmail.com>
2022-11-14 23:39:17 -0500

    some new functionality

--- app.py
+++ app.py
@@ -3,25 +3,30 @@ import random
 
 app = flask.Flask(__name__)
 
-possible_images = [
-    "/static/farquaad.gif",
-    "/static/donkey-smile.gif",
-    "/static/pinocchio-ginger-bread.gif",
-]
+# possible_images = [
+#     "/static/farquaad.gif",
+#     "/static/donkey-smile.gif",
+#     "/static/pinocchio-ginger-bread.gif",
+# ]
 
 
 @app.route("/")
 def index():
-    img = random.choice(possible_images)
+    # img = random.choice(possible_images)
     return flask.render_template(
         "index.html",
-        spooky_image=img,
+        # spooky_image=img,
     )
 
 
 @app.route("/swamp")
 def swamp():
     return flask.render_template("swamp.html")
+
+
+@app.route("/first")
+def first():
+    return flask.render_template("first.html")
 
 
 app.run()
